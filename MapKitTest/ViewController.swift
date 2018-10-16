2d0b8e21b213cb4ba3a65c045a7d102ee24e173d
Robert Wood <rwood366@gmail.com>
2018-10-16 20:12:28 +0100

    Initial Commit

---
+++ MapKitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKitTest
+//
+//  Created by Robert Wood on 16/10/2018.
+//  Copyright © 2018 Robert Wood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
